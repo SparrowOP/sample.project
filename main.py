ae59cf21db548df160a615c35d6b2bc119b983cf
Yash Yadav <93728582+SparrowOP@users.noreply.github.com>
2021-12-07 23:08:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+path = r"C:\Users\uschb\PythonProject\CSV\in.csv"
+df = pd.read_csv(path, skiprows=1, header=None)
+vol = df.to_numpy()
+mean = vol.mean()
+median = np.median(vol)
+sd = vol.std()
+print("The Median of the Volumes is :", median)
+print("The Mean of the Volumes is :", mean)
+print("The Standard Deviation of the Volumes is :", sd)
